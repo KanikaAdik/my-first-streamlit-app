b834361b10524f9da3ca2be9620018392ad85b9c
KanikaAdik <kanikaadik07@gmail.com>
2023-03-14 23:52:42 -0400

    update streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,36 +4,13 @@ import streamlit as st
 
 # Display title and text
 st.title("Week 1 - Data and visualization")
-st.markdown("Here we can see the dataframe created during this weeks project.")
 
-# Read dataframe
-dataframe = pd.read_csv(
-    "WK1_Airbnb_Amsterdam_listings_proj_solution.csv",
-    names=[
-        "Airbnb Listing ID",
-        "Price",
-        "Latitude",
-        "Longitude",
-        "Meters from chosen location",
-        "Location",
-    ],
-)
 
 # We have a limited budget, therefore we would like to exclude
 # listings with a price above 100 pounds per night
 dataframe = dataframe[dataframe["Price"] <= 100]
 
-# Display as integer
-dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
-# Round of values
-dataframe["Price"] = "£ " + dataframe["Price"].round(2).astype(str) # <--- CHANGE THIS POUND SYMBOL IF YOU CHOSE CURRENCY OTHER THAN POUND
-# Rename the number to a string
-dataframe["Location"] = dataframe["Location"].replace(
-    {1.0: "To visit", 0.0: "Airbnb listing"}
-)
 
-# Display dataframe and text
-st.dataframe(dataframe)
 st.markdown("Below is a map showing all the Airbnb listings with a red dot and the location we've chosen with a blue dot.")
 
 # Create the plotly express figure
@@ -54,3 +31,30 @@ fig.update_layout(mapbox_style="stamen-terrain")
 
 # Show the figure
 st.plotly_chart(fig, use_container_width=True)
+
+
+st.markdown("Here we can see the dataframe created during this weeks project.")
+
+# Read dataframe
+dataframe = pd.read_csv(
+    "WK1_Airbnb_Amsterdam_listings_proj_solution.csv",
+    names=[
+        "Airbnb Listing ID",
+        "Price",
+        "Latitude",
+        "Longitude",
+        "Meters from chosen location",
+        "Location",
+    ],
+)
+# Display as integer
+dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
+# Round of values
+dataframe["Price"] = "£ " + dataframe["Price"].round(2).astype(str) # <--- CHANGE THIS POUND SYMBOL IF YOU CHOSE CURRENCY OTHER THAN POUND
+# Rename the number to a string
+dataframe["Location"] = dataframe["Location"].replace(
+    {1.0: "To visit", 0.0: "Airbnb listing"}
+)
+
+# Display dataframe and text
+st.dataframe(dataframe)
